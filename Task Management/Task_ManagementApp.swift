6be7b1e39ca5cfd5c16669b811e55ef4b6980bf3
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2023-01-27 11:39:55 +0200

    Initial Commit

---
+++ Task Management/Task_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagementApp.swift
+//  Task Management
+//
+//  Created by KhaleD HuSsien on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
