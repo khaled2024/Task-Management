830a6add96fb27f9085204c95dce478d332e880e
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2023-01-30 15:27:05 +0200

    create new task view  and end the app

---
+++ Task Management/Model/TaskCategory.swift
@@ -0,0 +1,35 @@
+//
+//  TaskCategory.swift
+//  Task Management
+//
+//  Created by KhaleD HuSsien on 30/01/2023.
+//
+
+import SwiftUI
+
+enum Category: String,CaseIterable {
+    case general = "General"
+    case bug = "Bug"
+    case idea = "Idea"
+    case modifiers = "Modifiers"
+    case challenge = "Challenge"
+    case coding = "Coding"
+    
+    var color: Color{
+        switch self {
+        case .general:
+            return .gray
+        case .bug:
+            return .green
+        case .idea:
+            return .pink
+        case .modifiers:
+            return .blue
+        case .challenge:
+            return .purple
+        case .coding:
+            return .brown
+        }
+    }
+
+}
--- Task Management/View/Home.swift
+++ Task Management/View/Home.swift
@@ -9,12 +9,20 @@ import SwiftUI
 
 struct Home: View {
     @State private var currentDay: Date = .init()
+    @State private var tasks: [Task] = sampleTask
+    @State private var addNewTask:Bool = false
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            //
+            TimelineView()
+                .padding(15)
         }
         .safeAreaInset(edge: .top, spacing: 0) {
             headerView()
+        }
+        .fullScreenCover(isPresented: $addNewTask) {
+            AddTaskView { task in
+                tasks.append(task)
+            }
         }
     }
     //MARK: - Header Builder:-
@@ -31,7 +39,7 @@ struct Home: View {
                 }
                 .hAlign(.leading)
                 Button {
-                    
+                    addNewTask.toggle()
                 } label: {
                     HStack(spacing: 10) {
                         Image(systemName: "plus")
@@ -57,7 +65,18 @@ struct Home: View {
             WeekRow()
         }
         .padding(15)
-        
+        /// this background to make sure that the timeline date didnt shown in top of the headerview
+        .background(
+            VStack(spacing: 0, content: {
+                Color.white
+                // Gradient...
+                Rectangle()
+                    .fill(
+                        .linearGradient(colors: [.white,.clear], startPoint: .top, endPoint: .bottom))
+                    .frame(height: 20)
+            })
+            .ignoresSafeArea()
+        )
     }
     // Week row
     @ViewBuilder
@@ -67,9 +86,9 @@ struct Home: View {
                 let status = Calendar.current.isDate(weekDay.date, inSameDayAs: currentDay)
                 VStack(spacing: 6){
                     Text(weekDay.string.prefix(3))
-                        .ubuntu(12, .medium)
+                        .ubuntu(13, .medium)
                     Text(weekDay.date.toString(format: "dd"))
-                        .ubuntu(16,status ? .medium : .regular)
+                        .ubuntu(17,status ? .medium : .regular)
                 }
                 .foregroundColor(status ? Color.blue : .gray)
                 .hAlign(.center)
@@ -83,6 +102,81 @@ struct Home: View {
         }
         .padding(.vertical, 10)
         .padding(.horizontal, -15)
+    }
+    // timeline view...
+    @ViewBuilder
+    func TimelineView()-> some View{
+        ScrollViewReader{ proxy in
+            let hours = Calendar.current.hours
+            let midHour = hours[hours.count / 2]
+            VStack{
+                ForEach(hours,id: \.self){hour in
+                    TimelineViewRow(hour)
+                        .id(hour)
+                }
+            }
+            .onAppear {
+                proxy.scrollTo(midHour)
+            }
+        }
+    }
+    // timeline view row...
+    @ViewBuilder
+    func TimelineViewRow(_ date: Date)-> some View{
+        HStack(alignment: .top) {
+            Text(date.toString(format: "h a"))
+                .ubuntu(14, .regular)
+                .frame(width: 45, alignment: .leading)
+            
+            // filtering tasks...
+            let calender = Calendar.current
+            let filterTasks = tasks.filter {
+                if let hour = calender.dateComponents([.hour], from: date).hour, let taskHour = calender.dateComponents([.hour], from: $0.dateAdded).hour,hour == taskHour && calender.isDate($0.dateAdded, inSameDayAs: currentDay){
+                    return true
+                }
+                return false
+            }
+            if filterTasks.isEmpty{
+                Rectangle()
+                    .stroke(.gray.opacity(0.5),style: StrokeStyle(lineWidth: 0.5, lineCap: .butt, lineJoin: .bevel, dash: [5], dashPhase: 5))
+                    .frame(height: 0.5)
+                    .offset(y: 10)
+            }else{
+                // Task view...
+                VStack(spacing: 10) {
+                    ForEach(filterTasks) { task in
+                        TaskRow(task)
+                    }
+                }
+            }
+        }
+        .hAlign(.leading)
+        .padding(.vertical, 15)
+    }
+    // Task view...
+    @ViewBuilder
+    func TaskRow(_ task: Task)-> some View{
+        VStack(alignment: .leading, spacing: 8){
+            Text(task.taskName.uppercased())
+                .ubuntu(16, .regular)
+                .foregroundColor(task.taskCategory.color)
+            if task.taskDescription != ""{
+                Text(task.taskDescription)
+                    .ubuntu(14, .light)
+                    .foregroundColor(task.taskCategory.color.opacity(0.8))
+            }
+        }
+        .hAlign(.leading)
+        .padding(12)
+        .background(
+            ZStack(alignment: .leading){
+                Rectangle()
+                    .fill(task.taskCategory.color)
+                    .frame(width: 4)
+                Rectangle()
+                    .fill(task.taskCategory.color.opacity(0.25))
+            }
+        )
     }
 }
 
@@ -110,6 +204,18 @@ extension Date{
 }
 //MARK: - Calender Ex :-
 extension Calendar{
+    /// return 24 hours in a day...
+    var hours: [Date]{
+        let startOfDay = self.startOfDay(for: Date())
+        var hours: [Date] = []
+        for index in 0..<24{
+            if let date = self.date(byAdding: .hour,value: index, to: startOfDay){
+                hours.append(date)
+            }
+        }
+        return hours
+    }
+    /// return current week in Array format
     var currentWeek: [WeekDay]{
         guard let firstWeekDay = self.dateInterval(of: .weekOfMonth, for: Date())?.start else{return []}
         var week: [WeekDay] = []
@@ -117,7 +223,7 @@ extension Calendar{
             if let day = self.date(byAdding: .day,value: index, to: firstWeekDay){
                 let weekDaySymbol: String = day.toString(format: "EEEE")
                 let isToday = self.isDateInToday(day)
-                week.append(.init(string: weekDaySymbol, date: day))
+                week.append(.init(string: weekDaySymbol, date: day,isToday: isToday))
             }
         }
         return week
@@ -127,7 +233,7 @@ extension Calendar{
         var id: UUID = .init()
         var string: String
         var date: Date
-        var today: Bool = false
+        var isToday: Bool = false
     }
 }
 
